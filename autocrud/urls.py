2bdf0838b78716eb489e2fc702d9d6af8cee4883
Thatcher Clay <thatcherclay@snatchBook-2.local>
2011-09-08 22:39:37 -0400

    Cleanup of old files

--- autocrud/urls.py
+++ autocrud/urls.py
@@ -1,6 +1,6 @@
 from django.db.models import get_app, get_models
 from django.conf.urls.defaults import patterns, include, url
-from djangocrud import requests
+from autocrud import requests
 
 urlpatterns = patterns('',
     url(r'(?P<appname>.*?)/(?P<operation>(create|read|update|delete))/(?P<modelname>(\w+))', requests.handle_request),
